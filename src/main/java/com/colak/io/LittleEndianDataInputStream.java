365682af3925f24ca61c759a8d0acca87ccda836
Orçun Çolak <orcun.colak@hazelcast.com>
2025-02-04 19:26:36 +0300

    stream example

--- src/main/java/com/colak/io/LittleEndianDataInputStream.java
+++ src/main/java/com/colak/io/LittleEndianDataInputStream.java
@@ -1,5 +1,6 @@
 package com.colak.io;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
@@ -13,5 +14,29 @@ public class LittleEndianDataInputStream {
                 .order(ByteOrder.LITTLE_ENDIAN);
     }
 
-    
+    public byte readByte() throws IOException {
+        ensureRemaining(1);
+        return byteBuffer.get();
+    }
+
+    public short readShort() throws IOException {
+        ensureRemaining(2);
+        return byteBuffer.getShort();
+    }
+
+    public int readInt() throws IOException {
+        ensureRemaining(4);
+        return byteBuffer.getInt();
+    }
+
+    public long readLong() throws IOException {
+        ensureRemaining(8);
+        return byteBuffer.getLong();
+    }
+
+    private void ensureRemaining(int bytes) throws IOException {
+        if (byteBuffer.remaining() < bytes) {
+            throw new IOException("Insufficient bytes available to read");
+        }
+    }
 }
