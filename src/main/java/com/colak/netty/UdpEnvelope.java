ca6b428b43d42d043071b6c8482654aba4d01aa9
Orçun Çolak <orcun.colak@hazelcast.com>
2026-02-07 11:06:51 +0300

    more dev

--- src/main/java/com/colak/netty/UdpEnvelope.java
+++ src/main/java/com/colak/netty/UdpEnvelope.java
@@ -6,13 +6,23 @@ import lombok.RequiredArgsConstructor;
 import java.net.InetSocketAddress;
 
 @Getter
-@RequiredArgsConstructor
 public class UdpEnvelope<T> {
     private final T payload;
     private final InetSocketAddress socketAddress;
+    private final byte[] rawBytes;
 
     public UdpEnvelope(T payload, String ip, int port) {
         this(payload, new InetSocketAddress(ip, port));
+    }
+
+    public UdpEnvelope(T payload, InetSocketAddress socketAddress) {
+        this(payload, socketAddress, null);
+    }
+
+    public UdpEnvelope(T payload, InetSocketAddress socketAddress, byte[] rawBytes) {
+        this.payload = payload;
+        this.socketAddress = socketAddress;
+        this.rawBytes = rawBytes;
     }
 
     @Override
